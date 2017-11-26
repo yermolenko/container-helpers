0f3ad291cd4ced90b72f921ee9fe62c9c6c9760d
Alexander Yermolenko <yaa.mbox@gmail.com>
2017-11-26 21:56:14 +0200

    Add post-install script and patches for running LXDE inside Debian 9 container

---
+++ container-addons/debian-9-lxde/post-install-and-apps.sh
@@ -0,0 +1,40 @@
+apt-get update
+
+apt-get install bash-completion
+apt-get install locales
+dpkg-reconfigure locales
+
+dpkg-reconfigure tzdata
+
+apt-get install mc
+apt-get install htop
+
+chsh -s /bin/bash user
+
+apt-get install mesa-utils
+apt-get --no-install-recommends install lxde
+apt-get --no-install-recommends install pavucontrol
+
+apt-get install clearlooks-phenix-theme
+
+apt-get install dirmngr
+
+apt-get install x11-utils
+apt-get install xinput
+apt-get install x11-apps
+
+apt-get install firefox-esr
+
+apt-get --no-install-recommends install gdebi
+
+apt-get install python-gpgme
+apt-get install ca-certificates
+apt-get install libxslt1.1
+
+apt-get install synaptic
+
+apt-get install smplayer vlc
+apt-get install mplayer
+
+#apt-get install gnome-keyring
+apt-get install seahorse
